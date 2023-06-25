461ca96ab3d62eeb5978918a170c32271d7f3d94
mictlantecutli <moisesparra82@hotmail.com>
2023-06-24 18:53:25 -0600

    changes in swagger

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API Books',
     description: 'This API is a collection of books',
   },
-  host: 'lesson05-indiv.onrender.com',
+  host: 'lesson06-indiv.onrender.com',
   schemes: ['https'],
 };
 
