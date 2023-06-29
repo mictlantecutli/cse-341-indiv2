9ebb30a2889964e781138a3c752e58727f7cf5f7
mictlantecutli <moisesparra82@hotmail.com>
2023-06-29 17:23:50 -0600

    setting passport and name of the cookie

--- server.js
+++ server.js
@@ -13,23 +13,40 @@ const githubStrategy = require('passport-github2').Strategy
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 app.use(parser.json());
 
 const port = 8080;
 
+//set the cookie with name of: 'secret'
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true,
+}))
 
+
+//setting the passport object
+app.use(passport.initialize())
+
+app.use(passport.session())
 
 //const swaggerUi = require('swagger-ui-express');
 //const swaggerDocument = require('./swagger-output.json');
 
-
 app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers',
+  'Origin, X-Requested-with, Content-Type, Accept, Z-key, Authorization');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'POST, GET, PUT, PATCH, OPTIONS, DELETE'
+  );
   next();
   
 })
 
+app.use(cors({methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}));
+app.use(cors({origin: '*'}));
 
 app.use('/', require('./routes'));
 
